a1ecf326496d70569c75ea94c25ebc20171c3090
Ha Duc Kien <haduckien1709@gmail.com>
2024-08-29 22:43:59 +0700

    4x4

--- maphuong.py
+++ maphuong.py
@@ -1,15 +1,12 @@
 def generate_magic_square_4x4():
     n = 4
     magic_square = [[(i * n) + j + 1 for j in range(n)] for i in range(n)]
-
-    # Hoán đổi các phần tử
     for i in range(n):
         for j in range(n):
             if (i < n // 4 or i >= 3 * n // 4) and (j < n // 4 or j >= 3 * n // 4):
                 magic_square[i][j] = n * n + 1 - magic_square[i][j]
             elif (i >= n // 4 and i < 3 * n // 4) and (j >= n // 4 and j < 3 * n // 4):
                 magic_square[i][j] = n * n + 1 - magic_square[i][j]
-
     return magic_square
 
 def print_magic_square(square):
@@ -18,3 +15,6 @@ def print_magic_square(square):
 
 magic_square = generate_magic_square_4x4()
 print_magic_square(magic_square)
+
+
+
